07cb77bc65f080e26169f161f55a01d9fc79ef5c
Richalien <Richalien@yandex.ru>
2020-10-01 14:20:14 +0400

    формула евклида

--- src/main/Evklid/Evklid.js
+++ src/main/Evklid/Evklid.js
@@ -1,6 +1,6 @@
 import {evkl} from "../mathOperation/pow";
 import {distinction} from "../distinction";
-
+//евклид
 export let evklid = (x, y, city, xC, yC, cityC) => {
     return (Math.sqrt(evkl(x, xC)) + Math.sqrt(evkl(y, yC)) + distinction(city, cityC));
 }
