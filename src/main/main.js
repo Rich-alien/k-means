cd3b608d96b781b4ebb0ca8967b96d88c0641d2c
Richalien <Richalien@yandex.ru>
2020-10-01 14:18:28 +0400

    main

--- src/main/main.js
+++ src/main/main.js
@@ -3,7 +3,7 @@ import {createNewData} from './generatorObj'
 import {claster} from "./claster";
 import {EvklidCyrcle} from "./Evklid/EvklidCyrcle";
 import {cluster, FilterOperationDistant, operationDistant} from "./data";
-
+//главный файл для запуска необходимых функций
 export let run = () => {
     createNewData();
     getNormalXY();
