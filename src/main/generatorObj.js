f6412f7f223c942411c2b2622d7e4bcb675bf5f7
Richalien <Richalien@yandex.ru>
2020-10-01 14:17:42 +0400

    случайная генерация точек на холсте

--- src/main/generatorObj.js
+++ src/main/generatorObj.js
@@ -1,7 +1,7 @@
 import {City, Color, data, quantity} from './data';
 import {getRandomInt} from './GetRandom/GetRandomInt'
 import {getRandomFloat} from './GetRandom/GetRandomFloat'
-
+//генерация объектов
 export let createNewData = () => {
     for (let i = 0; i < quantity; i++) {
         let RandomNumber = getRandomInt(3);
