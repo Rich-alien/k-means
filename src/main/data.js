6d248f4e8057a6a3190d0a8e147a3788cf839a23
Richalien <Richalien@yandex.ru>
2020-10-07 03:37:19 +0400

    добавлены поля для сохранения значений подсчета дистанций и фильтра , где идет отбор минимального эллемента

--- src/main/data.js
+++ src/main/data.js
@@ -2,10 +2,14 @@
 export let clustersNum = 3;
 export let PARAM = 0.00001;
 export let data = [];
-export let AfterNormalizeData = [];
+export let AfterNormalizeData = [];//После тусы с нормализацией
 export let cluster = [];
 export let City = ['Тольятти', 'Самара', 'Чапаевск'];
-export let operationDistant =[]
+export let operationDistant =[] // Евклид наименьшие значения
+export let operationDistantManhetan =[] // Евклид наименьшие значения
 export let FilterOperationDistant=[];
+export let FilterOperationDistantMan=[];
 export let Color = ['green', 'purple', 'black'];
 export let quantity = 100;
+export let TimeSet1Sec = [];
+
