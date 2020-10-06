c92389905265902d6aed20ceee7e3111f0302a39
Richalien <Richalien@yandex.ru>
2020-10-07 03:39:55 +0400

    delete trash

--- src/main/main.js
+++ src/main/main.js
@@ -1,14 +1,10 @@
 import {getNormalXY} from "./GetNormalValue";
 import {createNewData} from './generatorObj'
 import {claster} from "./claster";
-import {EvklidCyrcle} from "./Evklid/EvklidCyrcle";
-import {cluster, FilterOperationDistant, operationDistant} from "./data";
+
 //главный файл для запуска необходимых функций
 export let run = () => {
     createNewData();
     getNormalXY();
     claster();
-    EvklidCyrcle();
-
-    console.log(FilterOperationDistant);
 }
