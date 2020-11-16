8af9c2a7bc513df3f334e1e2556a82512333ff96
Richalien <Richalien@yandex.ru>
2020-11-16 17:14:58 +0400

    +fix генерируются и город, без них никак

--- src/knn/generateVoidPoints.js
+++ src/knn/generateVoidPoints.js
@@ -1,16 +1,18 @@
-import {data, VoidPoints} from "../main/data";
+import {saveData, VoidPoints} from "../main/data";
+import {getRandomCity} from "../main/GetRandom/GetRandomCity";
 
 export let generateVoidPoints = () => {
     let newVoid = {}
-    for (let i = 0; i < 4; i++) {
-        newVoid = {
-            x: ((data[i + 30].x - 0.1) / (1 - 0.1)) * 700,
-            y: ((data[i + 40].y - 0.1) / (300 - 1)) * 700,
-            city: null,
-            color: '#FF69B4',
-
-        }
-        VoidPoints.push(newVoid);
-    }
+   for(let i=0;i<1000;i++){
+       for (let i = 0; i < 5; i++) {
+           newVoid = {
+               x: saveData[i + Math.floor(Math.random() * Math.floor(70))].x,
+               y: saveData[i + Math.floor(Math.random() * Math.floor(70))].y,
+               city: getRandomCity(),
+               color: '#FF69B4',
+           }
+           VoidPoints.push(newVoid);
+       }
+   }
     console.log(VoidPoints);
 }
