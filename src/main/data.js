5acb8821cbe4c5bc22364d3097ff00b3a16407d9
Richalien <Richalien@yandex.ru>
2020-11-16 17:15:21 +0400

    + void points

--- src/main/data.js
+++ src/main/data.js
@@ -12,6 +12,7 @@ export let FilterOperationDistantMan = [];
 export let Color = ['green', 'purple', 'black'];
 export let quantity = 100;
 export let TimeSet1Sec = [];
+export let VoidPoints =[];
 export let saveData = [
     {id: 0, x: 28.509279445729035, y: 341.571906354515, city: "Тольятти", color: "black"},
     {id: 1, x: 0.022111416550387362, y: 575.685618729097, city: "Самара", color: "purple"},
