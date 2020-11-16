cc020b5e3ae9dd45bf23f08400d842193220e46e
Richalien <Richalien@yandex.ru>
2020-11-16 17:39:30 +0400

    удален лишний цикл

--- src/knn/generateVoidPoints.js
+++ src/knn/generateVoidPoints.js
@@ -3,16 +3,14 @@ import {getRandomCity} from "../main/GetRandom/GetRandomCity";
 
 export let generateVoidPoints = () => {
     let newVoid = {}
-   for(let i=0;i<1000;i++){
-       for (let i = 0; i < 5; i++) {
-           newVoid = {
-               x: saveData[i + Math.floor(Math.random() * Math.floor(70))].x,
-               y: saveData[i + Math.floor(Math.random() * Math.floor(70))].y,
-               city: getRandomCity(),
-               color: '#FF69B4',
-           }
-           VoidPoints.push(newVoid);
-       }
-   }
-    console.log(VoidPoints);
+    for (let i = 0; i < 5; i++) {
+        newVoid = {
+            x: saveData[i + Math.floor(Math.random() * Math.floor(70))].x,
+            y: saveData[i + Math.floor(Math.random() * Math.floor(70))].y,
+            city: getRandomCity(),
+            color: '#FF69B4',
+        }
+        VoidPoints.push(newVoid);
+    }
+
 }
