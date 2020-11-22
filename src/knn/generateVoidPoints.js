29f76a14f6d45a5fb001c84fff1e39bcbd5f1ce5
Richalien <Richalien@yandex.ru>
2020-11-23 03:56:57 +0400

    доавлено условие , что генерация точек происходит только один раз

--- src/knn/generateVoidPoints.js
+++ src/knn/generateVoidPoints.js
@@ -1,16 +1,20 @@
 import {saveData, VoidPoints} from "../main/data";
 import {getRandomCity} from "../main/GetRandom/GetRandomCity";
-
-export let generateVoidPoints = () => {
+let click = 0;
+export const generateVoidPoints = () => {
     let newVoid = {}
-    for (let i = 0; i < 5; i++) {
-        newVoid = {
-            x: saveData[i + Math.floor(Math.random() * Math.floor(70))].x,
-            y: saveData[i + Math.floor(Math.random() * Math.floor(70))].y,
-            city: getRandomCity(),
-            color: '#FF69B4',
+    if (click === 0) {
+        for (let i = 0; i < 5; i++) {
+            click++;
+            newVoid = {
+                x: saveData[i + Math.floor(Math.random() * Math.floor(70))].x,
+                y: saveData[i + Math.floor(Math.random() * Math.floor(70))].y,
+                city: getRandomCity(),
+                color: '#FF69B4',
+            }
+            VoidPoints.push(newVoid);
         }
-        VoidPoints.push(newVoid);
+    }else{
+        console.log('nope');
     }
-
 }
